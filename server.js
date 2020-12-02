62fb17cbc770bcb847e1f1ff0fbf067c8e6a8a2b
aubrey-heim <aubrey.l.heim@gmail.com>
2020-12-02 15:50:45 -0800

    styling

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
