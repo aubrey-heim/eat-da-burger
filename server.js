46009f469b2fd0b58f5c3537c021d70403d0dca6
aubrey-heim <aubrey.l.heim@gmail.com>
2020-12-05 09:24:10 -0800

    added JawsDB server info

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
 require("dotenv").config();
 
-var app = express();
+const app = express();
 
-var PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,13 +14,20 @@ app.use(express.static(__dirname + '/public'));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
+
 
 connection.connect(function(err) {
   if (err) {
